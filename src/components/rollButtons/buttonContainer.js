40f8ca61955a4bfcdbb836c21f1fad0a5ef74d0e
Lewis James <lewiji@gmail.com>
2021-04-04 16:25:54 +0100

    chore: animate roll/reset buttons/bar in and out when selection is made or reset

--- src/components/rollButtons/buttonContainer.js
+++ src/components/rollButtons/buttonContainer.js
@@ -1,13 +1,36 @@
 import RollButton from "./rollButton";
 import ResetButton from "./resetButton";
 import * as PropTypes from "prop-types";
+import {motion} from "framer-motion";
+import useIsSelectionEmpty from "../../hooks/useIsSelectionEmpty";
+
+const variants = {
+    enabled: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            stiffness: 300,
+            damping: 30,
+            mass: 0.5,
+        }
+    },
+    disabled: {
+        y: 190,
+        opacity: 0
+    }
+}
 
 export default function ButtonContainer({onRoll}) {
-    return <div className="fixed bottom-0 left-0 w-full mx-auto flex justify-center gap-x-4 pb-8 items-end
-                            mt-5 z-50 bg-gradient-to-b from-transparent to-black via-gray-900">
-        <RollButton onRoll={onRoll}/>
-        <ResetButton/>
-    </div>;
+    const disabled = useIsSelectionEmpty();
+    return (
+        <motion.div variants={variants} initial={"disabled"} animate={disabled ? "disabled" : "enabled"}
+                    className="fixed bottom-0 left-0 w-full mx-auto flex justify-center gap-x-4 pb-8 items-end mt-5 z-50 bg-gradient-to-b
+                    from-transparent to-black via-gray-900">
+            <RollButton onRoll={onRoll}/>
+            <ResetButton/>
+        </motion.div>
+    );
 }
 
 ButtonContainer.propTypes = {
