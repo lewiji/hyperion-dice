7f38b8c6a924b31c89d5a0c0438fa982ed7ce03f
Lewis James <lewiji@gmail.com>
2021-04-05 10:58:49 +0100

    chore: limit results history to 10. add layout animation to results cards

--- src/components/results/resultCard.js
+++ src/components/results/resultCard.js
@@ -22,9 +22,10 @@ function ResultCard({body, reducer, ...props}) {
     if (props.v === undefined || props.v?.results === undefined) return null;
     return <>
         <motion.div className={`p-1.5 bg-gradient-to-tr from-gray-900 via-indigo-900 to-indigo-800 rounded-lg my-4 overflow-hidden`}
+                    layout
                     initial={{opacity: 0, scale: 0}}
                     animate={{opacity: 1, scale: 1}}
-                    exit={{opacity: 0, y: 100}}
+                    exit={{opacity: 0}}
                     transition={{duration: 0.2}}>
             <p className={"text-sm font-semibold ml-1 bg-gray-200 bg-opacity-5 w-min px-2 rounded shadow-sm text-shadow-sm"}>{props.v?.name}</p>
             <div className={"flex flex-row flex-wrap justify-center my-2 bg-purple-500 mx-1 bg-opacity-20 rounded overflow-hidden"}>
