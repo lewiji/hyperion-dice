d811561f83bc0b334874473730e0353ab0336a67
Lewis James <lewiji@gmail.com>
2021-04-05 10:27:37 +0100

    fix: crash if results history undefined

--- src/components/results/resultCard.js
+++ src/components/results/resultCard.js
@@ -24,11 +24,11 @@ function ResultCard({body, reducer, ...props}) {
                     animate={{opacity: 1, scale: 1}}
                     exit={{opacity: 0, y: 100}}
                     transition={{duration: 0.2}}>
-            <p className={"text-sm font-semibold ml-1 bg-purple-500 bg-opacity-40 w-min px-2 rounded shadow-sm text-shadow-sm"}>{props.v.name}</p>
+            <p className={"text-sm font-semibold ml-1 bg-gray-200 bg-opacity-5 w-min px-2 rounded shadow-sm text-shadow-sm"}>{props.v?.name}</p>
             <div className={"flex flex-row flex-wrap justify-center my-2 bg-purple-500 mx-1 bg-opacity-20 rounded overflow-hidden"}>
                 {props.v.results?.map(body)}
             </div>
-            <p className={"text-center p-1 bg-black bg-opacity-60 rounded-lg"}>
+            <p className={"text-center p-1 bg-black bg-opacity-50 rounded-lg"}>
                 {renderDataHumanReadable(props.v.results?.reduce(reducer, props.initialState))}</p>
         </motion.div>
     </>;
