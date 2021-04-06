65e98808d62896ad6ec4de401831d2edb844c927
Lewis James <lewiji@gmail.com>
2021-04-06 21:38:28 +0100

    chore: change result card background to different colours on success/failure/triumph

--- src/components/results/resultCard.js
+++ src/components/results/resultCard.js
@@ -1,6 +1,10 @@
 import * as PropTypes from "prop-types";
-import {memo} from "react";
+import {memo, useEffect, useState} from "react";
 import {motion} from "framer-motion";
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../../tailwind.config.js'
+
+const twConfig = resolveConfig(tailwindConfig)
 
 function renderDataHumanReadable(reduction) {
     if (reduction === undefined) return "";
@@ -11,34 +15,86 @@ function renderDataHumanReadable(reduction) {
     let out = [];
     if (triumph > 0) out.push(`${triumph} triumph`);
     if (force > 0) out.push(`${force} force`);
-    if (success > -1) out.push(`${success} success`);
-    else if (triumph < 1) out.push(`${Math.abs(success)} failure`)
+    if (success > 0) out.push(`${success} success`);
+    else if (triumph < 1 && success < 0) out.push(`${Math.abs(success)} failure`)
     if (advantage > 0) out.push(`${advantage} advantage`);
     else if (advantage < 0) out.push(`${Math.abs(advantage)} threat`);
+
+    if (out.length === 0) {
+        out.push("Nothing :(")
+    }
     return `${out.join(", ")}`;
 }
 
-function ResultCard({body, reducer, ...props}) {
-    if (props.v === undefined || props.v?.results === undefined) return null;
+const colours = {
+    base: twConfig.theme.colors.gray["900"],
+    success: twConfig.theme.colors.green["700"],
+    failure: twConfig.theme.colors.red["800"],
+    triumph: twConfig.theme.colors.yellow["500"]
+}
+
+const cardVariants = {
+    initial: {opacity: 0, scale: 0},
+    animate: {opacity: 1, scale: 1, backgroundColor: colours.base},
+    success: {
+        opacity: 1, scale: 1, backgroundOpacity: 0.9, backgroundColor: colours.success
+    },
+    failure: {
+        opacity: 1, scale: 1, backgroundOpacity: 0.9, backgroundColor: colours.failure
+    },
+    triumph: {
+        opacity: 1, scale: 1, backgroundOpacity: 0.9, backgroundColor: colours.triumph
+    },
+    exit: {opacity: 0}
+}
+
+
+function ResultCard({body, reducer, value, ...props}) {
+    const [readout, setReadout] = useState("");
+    const [variant, setVariant] = useState("animate");
+
+    useEffect(() => {
+        const hrString = renderDataHumanReadable(value?.results?.reduce(reducer, props.initialState));
+        setReadout(hrString);
+        if (hrString.includes("triumph")) {
+            setTimeout(() => {
+                setVariant("triumph");
+            }, 600);
+        } else if (hrString.includes("success")) {
+            setTimeout(() => {
+                setVariant("success");
+            }, 600);
+        } else if (hrString.includes("failure")) {
+            setTimeout(() => {
+                setVariant("failure");
+            }, 600);
+        }
+    }, [value]);
+
+    if (value === undefined || value?.results === undefined) return null;
     return <>
-        <motion.div className={`p-1.5 bg-gradient-to-tr from-gray-900 via-indigo-900 to-indigo-800 rounded-lg my-4`}
+        <motion.div className={`p-1.5 rounded-lg my-4`}
                     layout
-                    initial={{opacity: 0, scale: 0}}
-                    animate={{opacity: 1, scale: 1}}
-                    exit={{opacity: 0}}
-                    transition={{duration: 0.2}}>
-            <p className={"text-sm font-semibold ml-1 bg-gray-200 bg-opacity-5 w-min px-2 rounded shadow-sm text-shadow-sm"}>{props.v?.name}</p>
-            <div className={"flex flex-row flex-wrap justify-center my-2 bg-purple-500 mx-1 bg-opacity-20 rounded overflow-hidden"}>
-                {props.v?.results?.map(body)}
+                    variants={cardVariants}
+                    initial={"initial"}
+                    animate={variant}
+                    exit={"exit"}
+                    transition={{type: "spring", duration: 0.8}}>
+            <p className={"text-sm font-semibold ml-1 bg-gray-200 bg-opacity-5 w-min px-2 rounded shadow-sm text-shadow-sm"}>{value?.name}</p>
+            <div className={"flex flex-row flex-wrap justify-center my-2 bg-gray-200 mx-1 bg-opacity-20 rounded overflow-hidden"}>
+                {value?.results?.map(body)}
             </div>
-            <p className={"text-center p-1 bg-black bg-opacity-50 rounded-lg"}>
-                {renderDataHumanReadable(props.v?.results?.reduce(reducer, props.initialState))}</p>
+            <motion.p variants={cardVariants} initial={"initial"} animate={"animate"} exit={"exit"}
+                      transition={{delay: 0.5}}
+                      className={"text-center p-1 bg-black bg-opacity-50 rounded-lg"}>
+                {readout}
+            </motion.p>
         </motion.div>
     </>;
 }
 
 ResultCard.propTypes = {
-    v: PropTypes.any,
+    value: PropTypes.any,
     prop1: PropTypes.func,
     prop2: PropTypes.func,
     initialState: PropTypes.shape({
